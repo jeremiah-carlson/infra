a9eeb76cce314aeeaafb74d69478fc9e1ea379a0
jeremiah-carlson <87202597+jeremiah-carlson@users.noreply.github.com>
2024-04-20 02:31:37 -0400

    Test array iteration

--- azure/mocks/fs_mini/main.tf
+++ azure/mocks/fs_mini/main.tf
@@ -24,3 +24,18 @@ resource "azurerm_storage_table" "fs_mini_stc" {
   name                 = var.fs_mini.base_table_name
   storage_account_name = azurerm_storage_account.fs_mini_sta.name
 }
+
+resource "azurerm_storage_table_entity" "base_entities" {
+  for_each = {
+    for index, ent in local.ent :
+    vm.name => vm
+  }
+  storage_account_name = azurerm_storage_account.fs_mini_sta.name
+  table_name           = azurerm_storage_table.fs_mini_stc
+  partition_key        = "t1"
+  row_key              = each.value.key
+
+  entity = {
+    val = each.value.val
+  }
+}
